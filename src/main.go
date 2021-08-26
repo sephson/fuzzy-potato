e9723ba64894a65511fdafb9ab92a0fd66860808
sephson <disutj@gmail.com>
2021-08-26 14:09:38 +0100

    first commit

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+
+	"github.com/sephson/fuzzy-potato/controllers"
+)
+ 
+
+
+func main(){
+	r := httprouter.New() //r is a variable
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser )
+	r.POST("/user", uc.CreateUser )
+	r.DELETE("/user/:id", uc.DeleteUser )
+
+	http.ListenAndServe("localhost:8000", r)
+}
+
+
+func getSession() *mgo.Session{
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil{ //handling error
+		panic(err)
+	}
+	return s
+
+}
